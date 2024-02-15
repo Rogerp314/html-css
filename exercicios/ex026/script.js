6d7cdba2fd196077fcdc131b2ee5fb974293949f
Rogerp314 <rogep1915@gmail.com>
2024-02-15 13:57:06 -0300

    Exercicio pica

---
+++ exercicios/ex026/script.js
@@ -0,0 +1,16 @@
+let dados = document.querySelector('#central')
+function texto(){
+    dados.innerHTML='Talvez...'
+    dados.backgroundColor='pink'
+    dados.color='red'
+}
+function sair(){
+    dados.innerHTML='SIM'
+    dados.backgroundColor='gray'
+    dados.color='white'
+}
+function clique(){
+    dados.innerHTML='Não'
+    dados.style.backgroundColor='black'
+    dados.style.color='red'
+}
