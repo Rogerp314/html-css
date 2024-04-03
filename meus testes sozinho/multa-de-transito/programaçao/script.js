78836c32ebaf1328148ff569fdd781700535ede8
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-04-02 23:15:02 -0300

    Ajustes no estilo e eventos da página do site

---
+++ meus testes sozinho/multa-de-transito/programaçao/script.js
@@ -0,0 +1,13 @@
+let azul = document.querySelector('#azul')
+let verde = document.querySelector('#verde')
+let amarelo = document.querySelector('#amarelo')
+function entrar(){
+    azul.style.color='blue'
+    verde.style.color='green'
+    amarelo.style.color='yellow'
+}
+function sair(){
+    azul.style.color='black'
+    verde.style.color='black'
+    amarelo.style.color='black'
+}
