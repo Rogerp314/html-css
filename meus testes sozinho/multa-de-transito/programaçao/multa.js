9be13ab7adac3a51d2e2fedd1dc181cc23629328
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-04-14 14:12:08 -0300

    Ajustes

--- meus testes sozinho/multa-de-transito/programaçao/multa.js
+++ meus testes sozinho/multa-de-transito/programaçao/multa.js
@@ -1,6 +1,5 @@
 let nome = document.querySelector('#nome')
 let velocidade = document.querySelector('#velocidade')
-let sex = document.getElementsByName('sexo')
 let res = document.querySelector('#resposta')
 let multas = {fraca: '130,16', media:'195,23', forte: '880,41'}
 
@@ -12,29 +11,32 @@ function chave(event){
 }
 
 function resultado(){
-    if(sex[0].checked){
-    if(nome.value.lenght==0 || velocidade.value.lenght==0){
-        res.innerHTML='[ERROR] Preencha os dados corretamente para assim fazer a simulação.'
+    res.style.color='black'
+    if(velocidade.value==0 && nome.value==0){
+        res.innerHTML='[ERROR] Preencha os campos obrigatórios corretamente.'
         res.style.color='red'
         nome.focus()
-        velocidade.value=''
-    } else if(velocidade.value>=0){
-        res.innerHTML=`O senhor <strong>${nome.value}</strong> está dentro do limite de velocidade então não precisará pagar multa.`
-    } else if(velocidade.value<=120){
-        res.innerHTML=`O senhor <strong>${nome.value}</strong> por desobedecer a lei tera de pagar <strong>R$${multas.fraca}</strong> por ter passado do limite da pista. <br> Passou há <strong>${velocidade.value}km/h</strong>.`
-    } else if(velocidade.value<=150){
-        res.innerHTML=`O senhor <strong>${nome.value}</strong> por desobedecer a lei terá de pagar <strong>R$${multas.media}</strong> por ter passado do limite da pista. <br> Passou há <strong>${velocidade.value}km/h</strong>.`
+    } else if(velocidade.value==0){
+        res.innerHTML='[ERROR] Preencha os dados corretamente. Faltou preencher a <strong>velocidade</strong>'
+        velocidade.focus()
+        res.style.color='red'
+    } else if(nome.value==0){
+        res.innerHTML='[ERROR] Preencha os dados corretamente. Faltou preencher o <strong>nome</strong>.'
+        res.style.color='red'
+        nome.focus()
     } else{
-        res.innerHTML=`O senhor <strong>${nome.value}</strong> por desobedecer a lei terá de pagar <strong>R$${multas.forte}</strong> por ter passado do limite da pista. <br> Passou há <strong>${velocidade.value}km/h</strong>.`
+        let sex = document.getElementsByName('sexo')
+
+        if(sex[0].checked){
+            if(velocidade.value<=100){
+                res.innerHTML=`O senhor <strong>${nome.value}</strong> não levou multa pois respeitou o limite de velocidade da pista.`
+            } else if(velocidade.value<=120){
+                res.innerHTML=`O senhor <strong>${nome.value}</strong> ultrapassou o limite de velocidade da pista. <br> O senhor estava a <strong>${velocidade.value}km/h</strong> levara uma multa de <strong>R$${multas.fraca}</strong>.`
+            } else if(velocidade.value<=150){
+                res.innerHTML=`O senhor <strong>${nome.value}</strong> ultrapassou o limite de velocidade da pista. <br> O senhor estava a <strong>${velocidade.value}km/h</strong> levara uma multa de <strong>R$${multas.media}</strong> e <strong>5 pontos na carteira</strong>.`
+            } else if(velocidade.value>150){
+                res.innerHTML=`O senhor <strong>${nome.value}</strong> ultrapassou o limite de velocidade da pista. <br> O senhor estava a <strong>${velocidade.value}km/h</strong> levara uma multa de <strong>R$${multas.forte}</strong> e <strong>o carro será aprendido</strong>.`
+            }
+        }
     }
-}   else if(sex[1].checked){
-    if(velocidade.value<=100){
-        res.innerHTML=`A senhora <strong>${nome.value}</strong> está dentro do limite de velocidade então não precisará pagar multa.`
-    } else if(velocidade.value<=120){
-        res.innerHTML=`A senhora <strong>${nome.value}</strong> por desobedecer a lei tera de pagar <strong>R$${multas.fraca}</strong> por ter passado do limite da pista. <br> Passou há <strong>${velocidade.value}km/h</strong>.`
-    } else if(velocidade.value<=150){
-        res.innerHTML=`A senhora <strong>${nome.value}</strong> por desobedecer a lei terá de pagar <strong>R$${multas.media}</strong> por ter passado do limite da pista. <br> Passou há <strong>${velocidade.value}km/h</strong>.`
-    } else{
-        res.innerHTML=`A senhora <strong>${nome.value}</strong> por desobedecer a lei terá de pagar <strong>R$${multas.forte}</strong> por ter passado do limite da pista. <br> Passou há <strong>${velocidade.value}km/h</strong>.`
-    }
-}}
+}
