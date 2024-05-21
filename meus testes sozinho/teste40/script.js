fb0e989300a2ed1fd19832526133d1262b7b14ff
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-05-21 00:12:31 -0300

    Iframe
    Iframe são 'link' interativos que 'rodam' o site do link só que na sua página, sendo assim muito mais legal tendo opções de estilo e nem todos os sites deixam seu conteúdo exposto para o uso em iframes.

---
+++ meus testes sozinho/teste40/script.js
@@ -0,0 +1,11 @@
+let caixa = document.querySelector('#teste')
+let res = document.querySelector('#resposta')
+
+function cuzin(){
+    res.innerHTML='Gaby! Eu te amo!!!'
+    res.style.color='#fd0087'
+}
+
+function piroca(){
+    res.innerHTML=''
+}
